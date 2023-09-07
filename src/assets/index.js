c4467f05b4862a4107e2ffdf2469164797c5e8e2
Prime Mark #105 <42176307+PrimeMK105@users.noreply.github.com>
2023-09-07 13:39:52 +0500

    remove

--- src/assets/index.js
+++
@@ -1,9 +0,0 @@
-import background from "./background.gif";
-import loader from "./loader.gif";
-import connections from "./connections"
-const assets = {
-    background,
-    loader,
-    connections
-};
-export default assets;
